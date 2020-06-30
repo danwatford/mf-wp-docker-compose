f81d84afa0c48c0b20a7d04210f21fdae628c3cb
Daniel Watford <daniel@watfordconsulting.com>
2020-06-30 13:57:48 +0100

    Fixed script 01-replace-urls.sh to handle the case when there are no sql files to import.

--- wp-data/01-replace-urls.sh
+++ wp-data/01-replace-urls.sh
@@ -3,12 +3,17 @@
 
 # This script is expected to be sourced, therefore test path against arg $1 rather than $0.
 SCRIPT=$(readlink -f "$1")
+# If nothing found in $1 then fall back to $0.
+if [ -z "$SCRIPT" ]; then
+  SCRIPT=$(readlink -f "$0")
+fi
 SCRIPTPATH=$(dirname "$SCRIPT")
 
 echo "SCRIPT: $SCRIPT"
 echo "SCRIPTPATH: $SCRIPTPATH"
 
 for x in $SCRIPTPATH/*.sql; do
+    [ -e "$x" ] || continue
     echo "Processing SQL file: $x"
-	sed -i 's#https://broadstairsfolkweek.org.uk#http://localhost#g' $x
+    sed -i 's#https://broadstairsfolkweek.org.uk#http://localhost#g' "$x"
 done
